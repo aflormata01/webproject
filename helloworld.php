4b3ed0af5e850b4f007df55d9943202730fae3c5
aflormata01 <aflormata2000@gmail.com>
2017-09-09 13:29:38 +0800

    initial file

---
+++ helloworld.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>HELLO WORLD!</title>
+</head>
+<body>
+<?php
+
+$ctr;
+
+for( $i = 1; $i<7; $i++ ) {
+            
+			echo "<h".$ctr."> HELLO WORLD! </h".$ctr.">";
+         
+?>
+</body>
+</html>
